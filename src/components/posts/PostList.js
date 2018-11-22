5cabe4c8c5090255b7421e32d1505cb15a14335e
Raju Gangadhar Suranagi <rsuranagi@tekion.com>
2018-11-22 16:39:46 +0530

    Added missing fiels

---
+++ src/components/posts/PostList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PostData from '../../profiles/profiles.json';
+import { Link, Route } from 'react-router-dom';
+import Profile from '../profile';
+
+class PostList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile Page</h1>
+        {PostData.map((postDetail,index) => {
+          return <div className="container" key={postDetail.guid}>
+          <div className="row">
+            <div className = "col-sm-9">
+              <img src = {postDetail.picture} />
+              <Link to={`/profile/${postDetail.guid}`}>{postDetail.name}</Link>
+            </div>
+          </div>
+          </div>
+        })}
+      </div>
+    );
+  }
+}
+
+export default PostList;
