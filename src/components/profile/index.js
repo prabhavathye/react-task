6b8727d052b63528fae676b8ddfca4d9c5e17f74
prabhavathye <prabhavathyskk41@gmail.com>
2018-11-27 17:46:20 +0530

    Update index.js
    index file is updated with the view list.

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import PostData from '../../profiles/profiles.json';
 
 export default class Profile extends React.Component {
   render() {
+    
     console.log(this.props);
     return (
-      <div>
-        Profile details go here for guid: {this.props.match.params && this.props.match.params.id}
-      </div>
+       <div>
+       <h1>Profile Page</h1>
+       {PostData.map((postDetail,index) => {
+         
+         console.log(this.props.match.params);
+         return <div className="container" key={postDetail.index}>
+         <div className="row">
+           <div className = "col-sm-9">
+            <div> 
+            <p> Name: {postDetail.name} </p>
+            <p>Age:{postDetail.age} </p>
+            <p>Gender:{postDetail.gender}</p>
+             </div>
+           </div>
+         </div>
+         </div>
+       })}
+     </div>
+     
     )
   }
 }
-
